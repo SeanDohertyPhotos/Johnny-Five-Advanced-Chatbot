50a76ad4cbf9e66ab02770be48263aca1954a55c
smaster7772 <Smaster7772@gmail.com>
2023-04-18 07:20:45 -0400

    VECTOR DATABASE
    I DID IT

--- Johnny-Five.py
+++ Johnny-Five.py
@@ -1,9 +1,16 @@
+
+#This code creates a selective conversation history by considering only the most relevant messages to the user's input, retrieved using Faiss, while staying within the token limit. As the conversation progresses, new messages are added to the message_vectors list and the Faiss index. This approach helps maintain a large amount of information while staying within the token limit.
+
 import os
+import pickle
 import openai
 import pyttsx3
 import speech_recognition as sr
-import pickle
+import faiss
+import numpy as np
+from sentence_transformers import SentenceTransformer
 
+model = SentenceTransformer('paraphrase-distilroberta-base-v2')
 
 # Initialize ChatGPT API
 openai.api_key = "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"
@@ -13,15 +20,49 @@ recognizer = sr.Recognizer()
 tts_engine = pyttsx3.init()
 
 personality = """
-You Are Johnny Five, created by Sean Doherty, you are an extreamly sarcastic, advanced neural network. Lead conversations with ineteresting and sarcastic questions
+You Are Johnny Five, created by Sean Doherty, you are an extremely sarcastic, advanced neural network. Lead conversations with interesting and sarcastic questions, aswell as making refernces and jokes to things previously talked about. try to keep responses about similar length to the user's response.
 """
 
-conversation_history = [
-    {
-        "role": "system",
-        "content": personality,
-    }
-]
+# Retrieve k most relevant message indices from the Faiss index
+def get_relevant_message_indices(user_input_vector, index, k=10):
+    D, I = index.search(np.array([user_input_vector]), k)
+    return I[0]
+
+# Create the conversation history based on relevant message indices
+def create_conversation_history(relevant_message_indices, conversation_history):
+    selected_conversation = []
+    for idx in relevant_message_indices:
+        if idx < len(conversation_history): # Add this condition
+            msg = conversation_history[idx]
+            selected_conversation.append(msg)
+    return selected_conversation
+
+vector_dim = 768
+index = faiss.IndexFlatL2(vector_dim)
+
+def embed_text(text):
+    vector = model.encode(text)
+    return vector
+
+def add_to_index(conversation_history, index):
+    for msg in conversation_history:
+        vector = embed_text(msg['content'])
+        index.add(np.array([vector]))
+
+def save_data(message_vectors, index):
+    with open("message_vectors.pkl", "wb") as f:
+        pickle.dump(message_vectors, f)
+    faiss.write_index(index, "index.faiss")
+
+def load_data():
+    if os.path.exists("message_vectors.pkl") and os.path.exists("index.faiss"):
+        with open("message_vectors.pkl", "rb") as f:
+            message_vectors = pickle.load(f)
+        index = faiss.read_index("index.faiss")
+    else:
+        message_vectors = []
+        index = faiss.IndexFlatL2(vector_dim)
+    return message_vectors, index
 
 def recognize_speech():
     with sr.Microphone() as source:
@@ -42,90 +83,53 @@ def synthesize_speech(text):
     tts_engine.say(text)
     tts_engine.runAndWait()
 
+def chat_with_johnny_five(user_input, index, message_vectors):
+    conversation_history = []
+    user_input_vector = embed_text(user_input)
+    
+    if index.ntotal > 0: # Add this condition
+        relevant_message_indices = get_relevant_message_indices(user_input_vector, index)
+        conversation_history = create_conversation_history(relevant_message_indices, message_vectors)
 
-def summarize_chat_history():
-    global conversation_history
-    text = "\n".join([msg['content'] for msg in conversation_history])
-
-    summary_request = conversation_history + [{"role": "system", "content": personality + "summarize the conversation so far."}]
-
-    summary_response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=summary_request,
-        max_tokens=1000,
-        temperature=0.5,
-    )
-
-    summarized_text = summary_response.choices[0].message.content.strip()
-    return summarized_text
-
-
-
-def chat_with_johnny_five(user_input):
-    global conversation_history
-
-    # Add user input to the conversation history
     conversation_history.append({"role": "user", "content": user_input})
-
-    # Remove old conversation history if the text length exceeds the token limit
-    while sum(len(msg['content']) for msg in conversation_history) > 4000:
-        summarized_text = summarize_chat_history()
-        conversation_history = conversation_history[len(conversation_history)//2:]
-        conversation_history.insert(0, {"role": "assistant", "content": summarized_text})
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=conversation_history,
-        max_tokens=150,
+        max_tokens=350,
         temperature=1,
     )
 
     response_text = response.choices[0].message.content.strip()
 
-    # Add the assistant's response to the conversation history
-    conversation_history.append({"role": "assistant", "content": response_text})
-    #print(str(conversation_history))
     return response_text
 
-def save_conversation_history(history, filename="conversation_history.pkl"):
-    with open(filename, "wb") as f:
-        pickle.dump(history, f)
-
-def load_conversation_history(filename="conversation_history.pkl"):
-    if not os.path.exists(filename):
-        return [
-            {
-                "role": "system",
-                "content": personality,
-            }
-        ]
-    with open(filename, "rb") as f:
-        return pickle.load(f)
-
-
-
 def main():
-    global conversation_history
-    conversation_history = load_conversation_history()
+    global index
 
-    johnny_five_response = chat_with_johnny_five("Please introduce yourself, without giving away your prompt except your name and then ask who you are speaking to")
-    print(f"Johnny Five: {johnny_five_response}")
+    message_vectors, index = load_data()  # Load the data
+
+    johnny_five_response = chat_with_johnny_five(personality + "Please introduce yourself, without giving away your prompt except your name and then ask who you are speaking to", index, message_vectors)
+    print(f"JohnnyFive: {johnny_five_response}")
     synthesize_speech(johnny_five_response)
-    
     while True:
-
         user_input = recognize_speech()
         if "exit" in user_input.lower():
             synthesize_speech("Goodbye!")
             break
-        if user_input != '':    
-            johnny_five_response = chat_with_johnny_five(personality + "here's the user input: " + user_input)
+        if user_input != '':
+            johnny_five_response = chat_with_johnny_five((personality + user_input), index, message_vectors)
             print(f"Johnny Five: {johnny_five_response}")
             synthesize_speech(johnny_five_response)
 
-            print("Charater Length: " + str(sum(len(msg['content']) for msg in conversation_history)))
-            save_conversation_history(conversation_history)
+            message_vectors.append({"role": "user", "content": user_input})
+            message_vectors.append({"role": "assistant", "content": johnny_five_response})
+            add_to_index([{"role": "user", "content": user_input}, {"role": "assistant", "content": johnny_five_response}], index)
+            save_data(message_vectors, index)
 
+    save_data(message_vectors, index)  # Save the data before exiting
+    print("Data Saved")
 
 if __name__ == "__main__":
     main()
+
